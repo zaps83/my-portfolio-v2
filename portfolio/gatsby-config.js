08307775cdecb293d312b6ddbfe699017110162b
Steven Zapart <steven.zapart@gmail.com>
2021-05-07 03:28:04 -0500

    change about page and logo color

--- portfolio/gatsby-config.js
+++ portfolio/gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/lightning-z-noname.PNG`, // This path is relative to the root of the site.
+        icon: `src/images/lightning-z-noname-blue.PNG`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
